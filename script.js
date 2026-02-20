a041157450f34545a809da4eceabedfc7d236e63
nuriagarcias <nuriagarcias@alumnat.iesdamiahuguet.net>
2026-02-20 10:16:45 +0100

    principio

--- script.js
+++ script.js
@@ -3,6 +3,17 @@
     Mostra el color hexadecimal passat per paràmetre
  */
 
+const colors= ['#ffd1dc', '#ffef96', '#aaff80']
+const sequenciaSimon= []
+let elegit = ""
+
+function iniciarPartida(){
+    numAleatori = Math.floor(Math.random() * colors.length);
+    elegit = colors[numAleatori]
+    sequenciaSimon.push(elegit);
+    showNotification(elegit);
+}
+
 function showNotification(message) {
     console.log(message)
     var notification = document.getElementById('notification');
@@ -12,4 +23,8 @@ function showNotification(message) {
     setTimeout(function() {
         notification.style.display = 'none';
     }, 2000);
+}
+
+function comprovar(color){
+    console.log(color)
 }
